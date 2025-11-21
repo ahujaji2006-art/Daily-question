64c601c704eec4147f695fa5ace0c15df6ccaec2
ahujaji2006-art <ahujaji2006@gmail.com>
2025-11-22 01:39:34 +0530

    Initial commit: added app.js

---
+++ 18+ages.js
@@ -0,0 +1,7 @@
+let ages = [12, 17, 20, 14, 25, 30];
+
+for (let i = 0; i < ages.length; i++) {
+    if (ages[i] >= 18) {
+        console.log(ages[i]);
+    }
+}
---
+++ morethan3letterword.js
@@ -0,0 +1,7 @@
+let words = ["apple", "cat", "banana", "a", "grapes"];
+
+for (let i = 0; i < words.length; i++) {
+    if (words[i].length > 3) {
+        console.log(words[i]);
+    }
+}
